08afa3b1df487a859fc7c0c1c816263633cba792
p942005405 <2000pan2000@163.com>
2016-10-25 00:42:15 +0800

    data：25/10/2016,author:coder:panpan,add data/costmap.mat,mod plot_grid_map.m

--- src/plot_grid_map.m
+++ src/plot_grid_map.m
@@ -1,3 +1,9 @@
-costmap_index = 
-size_x=max(costmap(:,1));
-size_y=max(costmap(:,2));
+clear all; clc;close all;
+path(path, '../data');
+load('costmap.mat');
+costmap_index = find(costmap(:,5)==254);
+world_axes = costmap(:,1:4);
+word_xy = world_axes(costmap_index,:);
+% costmap_index = 
+% size_x=max(costmap(:,1));
+% size_y=max(costmap(:,2));
