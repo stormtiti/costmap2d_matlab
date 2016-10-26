f9e811b4a114735dc9ca3216ece1cc66e5a07a78
p942005405 <2000pan2000@163.com>
2016-10-26 22:35:11 +0800

    date:26/10/2016,author:panpan,mod:add the code of draw boundry

--- src/plot_grid_map.m
+++ src/plot_grid_map.m
@@ -1,10 +1,17 @@
 clear all; clc;close all;
+
+resolution = 20;
 path(path, '../data');
 load('costmap.mat');
-size_x=max(costmap(:,1));
-size_y=max(costmap(:,2));
-resolution = 20
+size_x = max(costmap(:,1));
+size_y = max(costmap(:,2));
+Wx_min = min(costmap(:,3))-0.025;
+Wx_max = max(costmap(:,3))-0.025;
+Wy_min = min(costmap(:,4))-0.025;
+Wy_max = max(costmap(:,4))-0.025;
+
 map = robotics.BinaryOccupancyGrid(size_x,size_y,resolution,'grid');
+map.GridLocationInWorld = [Wx_min Wy_min];
 costmap_index = find(costmap(:,5)==254);
 world_axes = costmap(:,3:4);
 xy = world_axes(costmap_index,:)-0.05;
@@ -16,11 +23,16 @@ hold on
 plot(xy(:,1),xy(:,2),'.r')
 hold on
 grid on
-plot([-1.5 -8.6],[6.5 -0.6],'*r','MarkerSize',20);
- set(gca,'XTick',-1.475:0.25:6.475,'YTick',-8675:0.25:-0.625)
-axis equal;
-% xlim([-2 8])
-% ylim([-9 -1])
+rectangle('position',[Wx_min,Wy_min,Wx_max-Wx_min,Wy_max-Wy_min],'EdgeColor','b');
+% plot(-1.5 ,-8.6,'*r','MarkerSize',20);
+% plot(6.5 , -0.6,'*r','MarkerSize',20);
+% plot(-1.5 ,-0.6,'*r','MarkerSize',20);
+% plot(6.5 , -8.6,'*r','MarkerSize',20);
+
+set(gca,'XTick',-1.475:0.25:6.475,'YTick',-8.675:0.25:-0.625)
+% axis equal;
+xlim([-2 8])
+ylim([-9 0])
 
 
 
